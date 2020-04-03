34824ec3a81c4c76424f7da87eead95bc42da2a2
Mário Veronesi <marioinfo@oi.com.br>
2020-04-03 06:25:02 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+<nav id="menu">
+    <div>
+        <h1><a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_url'); ?>/img/ccbr_preto.svg"></a></h1>
+        <ul class="menu">
+            <a class="close-menu"><img width="30" src="<?php bloginfo('template_url'); ?>/img/close.svg"></a>
+            <li><a class="active">Estamos nessa juntos</a></li>
+            <li><a>Como estamos atuando</a></li>
+            <li><a>O que estamos fazendo</a></li>
+            <li><a>Onde estamos atuando</a></li>
+            <li><a>Depoimentos</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container-fluid">
+    <div class="row capa" id="capa">
+        <div class="container">
+            <a class="open-menu"><img width="40" src="<?php bloginfo('template_url'); ?>/img/bars.svg"></a>
+            <h1>Estamos nessa juntos</h1>
+            <img class="logo" src="<?php bloginfo('template_url'); ?>/img/ccbr_branco.svg">
+            <p>Diante do avanço do novo coronavírus em todo o mundo, como não poderia deixar de ser, temos tomado medidas que acreditamos ser capazes de ajudar as pessoas a atravessar essa crise.</p>
+            <p>A The Coca-Cola Company, nossos parceiros engarrafadores e a The Coca-Cola Foundation vão doar mais de 120 milhões de dólares em apoio aos esforços de prevenção e reação à Covid-19 nas comunidades afetadas.</p>
+            <p>Aqui no Brasil, estamos contribuindo com bebidas e itens necessários aos trabalhadores da saúde, apoio a comunidades vulneráveis e amplificação das mensagens de prevenção. Vamos usar esse espaço para contar mais detalhadamente o que temos feito diante desse cenário desafiador.</p>
+            <p>Acompanhe:</p>
+        </div>
+    </div>
+    <div class="row como" id="como">
+        <div class="col">
+            <h2>Como estamos atuando</h2>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*SCRIPTS HERE*/
+$(document).ready(function(e) {
+	/******************* MENU ********************/
+    $(".open-menu, .close-menu").click(function(){
+       $("#menu ul.menu").slideToggle();
+    });
+});
